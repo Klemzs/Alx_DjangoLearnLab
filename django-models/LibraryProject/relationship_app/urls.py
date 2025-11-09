733676ddbe78cf08198a894b87af560641786f55
Clement Odeh <odehclementonwu@gmail.com>
2025-11-09 23:20:23 +0100

    Add new views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, admin_view, librarian_view, member_view
 from . import views
 
 urlpatterns = [
@@ -8,6 +8,12 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
+
+    # Role based URLs
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
+
 
     # Function-based view URL pattern
     path('books/', list_books, name='book_list'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from .models import Library, Book
+from django.contrib.auth.decorators import user_passes_test
+from .models import Library, Book, UserProfile
+
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and hasattr(user, 'profile') and user.profile.role == 'Member'
 
 def list_books(request):
     books = Book.objects.all().order_by('title')
@@ -24,4 +34,16 @@ def register(request):
             return redirect('book_list')  # Redirect to book list after registration
     else:
         form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})    
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+@user_passes_test(is_admin, login_url='/relationship/login/')
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian, login_url='/relationship/login/')
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member, login_url='/relationship/login/')
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
