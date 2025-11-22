a978163ce298d8f64f8c328700beeb36a3e99bd1
Clement Odeh <odehclementonwu@gmail.com>
2025-11-22 12:57:31 +0100

    Add Authentication and permission

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -104,6 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': [
             'rest_framework.authentication.TokenAuthentication',
+            ],
+        'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permission.IsAuthenticated'
             ]
 }
 
