8398786ed0907658854278e8f231dd8c92ca05bd
Clement Odeh <odehclementonwu@gmail.com>
2025-12-02 18:54:50 +0100

    Add urls and new views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,7 +6,12 @@ from .views import (
     profile, 
     profile_update, 
     change_password,
-    home
+    home,
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
 )
 
 urlpatterns = [
@@ -19,5 +24,12 @@ urlpatterns = [
 
         # Add Profile management URLs
         path('profile/update/', profile_update, name = 'profile_update'),
-        path('profile/change-password/', change_password, name = 'change_paswword'),
+        path('profile/change-password/', change_password, name = 'change_password'),
+
+        # CRUD URLs for blog posts
+        path('posts/', PostListView.as_view(), name='post_list'),
+        path('posts/new/', PostCreateView.as_view(), name='post_create'),
+        path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+        path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+        path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
