f6025695783827fac3e2f834e24346d6632a139a
Clement Odeh <odehclementonwu@gmail.com>
2025-12-02 18:00:21 +0100

    Add CRUD operations

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth import login, authenticate, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
@@ -8,6 +10,48 @@ from django import forms
 from .models import Post
 from django.contrib import messages
 from django.urls import reverse_lazy
+
+# ListView to display all blog posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+# DetailView to show individual blog posts
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+# CreateView to allow authenticated users to create new posts
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# UpdateView to enable post authors to edit their posts
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# DeleteView to let authors delete their posts
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/posts/'
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 # Create your views here.
 class ExtendedUserCreationForm(UserCreationForm):
