c716e95eb5fc7d49857a0ee206eadc8eecc4ca81
Clement Odeh <odehclementonwu@gmail.com>
2025-12-02 18:58:38 +0100

    Add urls

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -28,8 +28,8 @@ urlpatterns = [
 
         # CRUD URLs for blog posts
         path('posts/', PostListView.as_view(), name='post_list'),
-        path('posts/new/', PostCreateView.as_view(), name='post_create'),
+        path('post/new/', PostCreateView.as_view(), name='post_create'),
         path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-        path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
-        path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+        path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
+        path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
