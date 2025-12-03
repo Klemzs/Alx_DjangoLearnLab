4a21bb7c4c6694b2de4b5a1bcb58ed636df89832
Clement Odeh <odehclementonwu@gmail.com>
2025-12-04 00:17:20 +0100

    Add Tag model

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'taggit',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'  # or 'post_list' or 'profile'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_URL = 'login'
