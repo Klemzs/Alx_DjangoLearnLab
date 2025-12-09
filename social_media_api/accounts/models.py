231ee3bcde0044d98a8bfe701d6871e977efae4b
Clement Odeh <odehclementonwu@gmail.com>
2025-12-09 23:31:41 +0100

    add model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,4 +6,4 @@ class CustomUser(AbstractUser):
     bio = models.TextField(blank = True)
     profile_picture = models.ImageField(upload_to = 'profile_pics', blank = True)
     followers = models.ManyToManyField('self', symmetrical = False, blank = True)
-
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followed_by', blank=True)
