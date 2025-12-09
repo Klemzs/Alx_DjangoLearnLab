dba171b184640434fbb8253d1f7b504f5e07cf5b
Clement Odeh <odehclementonwu@gmail.com>
2025-12-10 00:10:30 +0100

    Add views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, genericsi
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post, Comment
@@ -34,3 +34,15 @@ class CommentViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    pagination_class = PostPagination
+    
+    def get_queryset(self):
+        user = self.request.user
+        # Get users that the current user is following
+        following_users = user.following.all()
+        # Get posts from followed users, ordered by creation date (most recent first)
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
